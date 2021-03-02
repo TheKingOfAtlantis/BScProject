0a7659cdbedef4b1fac1b92d063043613a37db98
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-03-02 00:39:14 +0000

    QC: Removed dependency on rpy2
    Can't seen to install it on the uni server
    Using call to subprocess to call the script
    Moved plotting into R using ggplot2

--- scripts/QC-GenomeVsGenes.R
+++ scripts/QC-GenomeVsGenes.R
@@ -2,26 +2,51 @@
 # Use Mahalanobis Distance to find those that deviate significantly from the mean (3 sd)
 # Mahalanobis Distance generatlisation of Z-score (measure of the distance from mean)
 
-archaea  <- read.csv(file = "data/qc/count/archaea.csv", header = TRUE, row.names = 1)
-bacteria <- read.csv(file = "data/qc/count/archaea.csv", header = TRUE, row.names = 1)
+# Pulled this from: https://stackoverflow.com/a/39051803/3856359
+requiredPackages = c('tidyverse','ggplot2','ggrepel', 'magrittr')
+for(p in requiredPackages){
+  if(!require(p,character.only = TRUE)) install.packages(p)
+  library(p,character.only = TRUE)
+}
 
-archaea$dist = mahalanobis(
-    archaea,
-    colMeans(archaea),
-    cov(archaea)
+process = function(path) {
+    data <- read.csv(file = path, header = TRUE, row.names = 1)
+    data$dist = mahalanobis(
+        data,
+        colMeans(data),
+        cov(data)
+    )
+    data$pvalue = pchisq(
+        data$dist,
+        df=1,
+        lower.tail=FALSE
+    )
+    data$id = row.names(data)
+
+    data %>% mutate(color = case_when(
+        dist > 3 ~ "Outliers",
+        T ~ "Values"
+    )) %>% ggplot(aes(x = size/10^6, y = count, color = factor(color))) +
+        coord_cartesian() +
+        geom_point() +
+        geom_text_repel(data = . %>% filter(dist > 3), aes(label = id)) +
+        labs(
+            x = "Genome Size (Gbp)",
+            y = "Frequency of Genes",
+            color = "Legend"
+        )
+}
+
+process("data/qc/count/archaea.csv")
+ggsave(
+    "plot/qc/count/archaea.png",
+    width = 16,
+    height = 9
 )
-archaea$pvalue = pchisq(
-    archaea$dist,
-    df=1,
-    lower.tail=FALSE
-)
-bacteria$dist = mahalanobis(
-    bacteria,
-    colMeans(bacteria),
-    cov(bacteria)
-)
-bacteria$pvalue = pchisq(
-    bacteria$dist,
-    df=1,
-    lower.tail=FALSE
+
+process("data/qc/count/bacteria.csv")
+ggsave(
+    "plot/qc/count/bacteria.png",
+    width = 16,
+    height = 9
 )
--- scripts/QC-GenomeVsGenes.py
+++ scripts/QC-GenomeVsGenes.py
@@ -27,32 +27,7 @@ if __name__ == "__main__":
     # Use Mahalanobis Distance to find those that deviate significantly from the mean (3 sd)
     # Mahalanobis Distance generatlisation of Z-score (measure of the distance from mean)
 
-    from rpy2 import robjects
-    from rpy2.robjects import pandas2ri
-    robjects.pandas2ri.activate()
-    robjects.r.source('scripts/QC-GenomeVsGenes.R')
-    archaea  = robjects.conversion.rpy2py(robjects.r["archaea"])
-    bacteria = robjects.conversion.rpy2py(robjects.r["bacteria"])
+    print("Running the R script via subprocess.call(...)")
 
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots(figsize=(16, 9))
-    archaea.plot(kind="scatter", x="size", y="count", ax=ax)
-    archaea[archaea["dist"] > 3].plot(kind="scatter", x="size", y="count", ax=ax, color="orange", label="outliers")
-    archaea[archaea["dist"] > 3].apply(lambda x: ax.annotate(
-        text   = x.name,
-        xy     = x[["size", "count"]],
-        xytext = (5, -1),
-        textcoords="offset points"
-    ), axis=1)
-    fig.savefig("plot/qc/count/archaea.png")
-
-    fig, ax = plt.subplots(figsize=(16, 9))
-    bacteria.plot(kind="scatter", x="size", y="count", ax=ax)
-    bacteria[bacteria["dist"] > 3].plot(kind="scatter", x="size", y="count", ax=ax, color="orange", label="outliers")
-    bacteria[bacteria["dist"] > 3].apply(lambda x: ax.annotate(
-        text   = x.name,
-        xy     = x[["size", "count"]],
-        xytext = (5, -4),
-        textcoords="offset points"
-    ), axis=1)
-    fig.savefig("plot/qc/count/bacteria.png")
+    import subprocess
+    subprocess.call("Rscript scripts/QC-GenomeVsGenes.R")

4b9affba20b7fef46a7ac6e402aa6f7744331df1
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-03-02 16:20:27 +0000

    Unique Set of IDs - Extensible Too
    New method provides a ordered set of ID types which can be extracted to uniquely
    identify each gene, the order is linked to number of genes which can be identified.
    The first ID type can identify the most genes, the second identifies the most of
    out of the ones which cannot yet be identified, etc.
    
    The extra parameter can be used to pass a list of different feature types for which
    to generate a list of IDs to identify these features
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- scripts/QC-AvaliableIDs.py
+++ scripts/QC-AvaliableIDs.py
@@ -5,6 +5,8 @@
 #  - Shrink the list available in each genome down (w/i the seperate processes)
 #  - Shrink the list available across all genomes
 
+import functools, collections, itertools
+
 def pullGeneIds(feature):
     idTypes = []
     qualifiers = feature.qualifiers
@@ -19,24 +21,37 @@ def pullGeneIds(feature):
         for id in qualifiers["db_xref"]:
             idTypes.append(id.split(":")[0])
 
-    return set(idTypes)
+    return set(idTypes) # Keep as set so have unique set of ID types
 
-def shrinkGenomeIds(file):
+def shrinkGenomeIds(file, toFind):
     # First we store everything we get (even repeats)
     # Then we reduce
     from Bio import SeqIO
 
-    idTypes = []
     record = next(SeqIO.parse(file, "embl"))
-    for feature in filter(lambda x: x.type in "CDS", record.features):
-        idTypes.append(pullGeneIds(feature))
-
-    return functools.reduce(lambda x,y: x & y, idTypes)
+    return [pullGeneIds(feature) for feature in filter(lambda x: x.type in toFind, record.features)]
 
 from common import loadGlob
-import functools
 if __name__ == "__main__":
-    idTypes = loadGlob("data/genomes/*/*.embl", shrinkGenomeIds)
-    print(idTypes)
-    idTypes = functools.reduce(lambda x,y: x & y, idTypes)
-    print(idTypes)
+    idTypes = loadGlob("data/genomes/*/*.embl", shrinkGenomeIds, extra=["CDS"])
+
+    # Plan to find minimal set
+    # 1) Count frequence of each Id types usage
+    # 2) Select highest used ID type
+    # 3) Remove all which contain the ID type (these can be addressed with an ID of that type)
+    # 4) Repeat on set of unaddressable genes, until all can be addressed
+
+
+    # Use an OrderedDict (as an OrderedSet doesn't exist)
+    # Use the order to keep record of how many elements can be addressed
+    # With the first able to address the most - Order will be used to determine
+    # in what order to check for each ID
+    minimal = collections.OrderedDict()
+    remains = list(itertools.chain.from_iterable(idTypes))
+
+    while len(remains) != 0:
+        count = collections.Counter(itertools.chain.from_iterable(remains))
+        minimal[count.most_common(1)[0][0]] = None
+
+        remains = list(filter(lambda x: set(minimal.keys()).isdisjoint(x), remains))
+    print(list(minimal.keys()))

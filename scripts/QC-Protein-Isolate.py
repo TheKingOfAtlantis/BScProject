f5126def77581f458e7f47b87d9f1e72927de871
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-03-02 20:18:05 +0000

    Isolated Pseudogenes
    Was mentioned in the last meeting that psuedogenes will need to be removed (yet
    to do) as they will effect the results. Checked to see if they explain the large number
    of genes to fail QC check, which they did! Although a few oddities still persist,
    such as genes will unusual start codons
    
    Will need to use this info to update the other scripts to remove psuedogenes
    
    Added: Ability to find and remove pseudogenes
    Updated: Refactored code to make it easier to read
    Removed: Unnecessary __name__ == "__main__"
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- scripts/QC-Protein-Isolate.py
+++ scripts/QC-Protein-Isolate.py
@@ -1,31 +1,48 @@
 import pandas as pd
 from Bio import SeqIO
+import itertools, json
 
 def isolate(df):
     ''' Isolates those which failed '''
     inv = ~df # Invert true <=> false
     return df[inv.any(axis=1)]
 
-if __name__ == "__main__":
-    import itertools, json
-
-    with open("data/qc/proteins/archaea.json") as file:
-        archaea = json.load(file)
-        archaea = pd.concat({
+def getData(name):
+    with open(f"data/qc/proteins/{name}.json") as file:
+        data = json.load(file)
+        data = pd.concat({
             k:pd.DataFrame(v).drop("position", axis = 1)
-            for k,v in archaea.items()
+            for k,v in data.items()
         })
-        archaea = isolate(archaea)
+        return isolate(data)
 
-    with open("data/qc/proteins/bacteria.json") as file:
-        bacteria = json.load(file)
-        bacteria = pd.concat({
-            k:pd.DataFrame(v).drop("position", axis = 1)
-            for k,v in bacteria.items()
-        })
-        bacteria = isolate(bacteria)
+archaea  = getData("archaea")
+bacteria = getData("bacteria")
 
-    pd.concat({
-        "bacteria": bacteria,
-        "arachaea": archaea
-    }).to_csv("data/qc/proteins/failed.csv")
+pd.concat({
+    "bacteria": bacteria,
+    "arachaea": archaea
+}).to_csv("data/qc/proteins/failed.csv")
+
+def findPseudo(path, df):
+    # Check which genes that we encountered are psuedo genes
+    isPseudo = []
+    for genome in set(df.index.get_level_values(0)):
+        with open(f"{path}/{genome.split('.')[0]}.embl") as file:
+            record   = next(SeqIO.parse(file, "embl"))
+            features = list(filter(lambda x: x.type in "CDS", record.features))
+            for pos in df.loc[genome].index:
+                if("pseudo" in features[pos].qualifiers):
+                    isPseudo.append((genome, pos))
+    return isPseudo
+
+print("Fails w/ Psuedo")
+print(pd.concat({
+    "Archaea":  (~archaea).sum(),
+    "Bacteria": (~bacteria).sum()
+}, axis = 1))
+print("Fails w/o Psuedo")
+print(pd.concat({
+    "Archaea":  (~archaea.drop(findPseudo("data/genomes/archaea", archaea))).sum(),
+    "Bacteria": (~bacteria.drop(findPseudo("data/genomes/bacteria", bacteria))).sum()
+}, axis = 1))

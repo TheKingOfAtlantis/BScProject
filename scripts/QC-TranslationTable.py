d95293ce160e47a3d84a43d5ad2edd6693d854af
Sulaiman Sulaiman <ss2980@bath.ac.uk>
2021-03-22 18:24:42 +0000

    Translation Table Refactoring + Summary
    Added:
     - Plot summarising data from both bacteria and archaea
    Updated:
     - Extracted distribution plotting code into plotDistribution(...)
     - Changed plot size
    Signed-off-by: Sulaiman Sulaiman <ss2980@bath.ac.uk>

--- scripts/QC-TranslationTable.py
+++ scripts/QC-TranslationTable.py
@@ -28,34 +28,41 @@ from common import loadGlob
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def plotDistribution(data):
+    fig, ax = plt.subplots(1, len(data.columns), sharey=True)
+    for i, table in enumerate(data.columns):
+        values = data[table].sort_values()
+        if(len(data.columns) == 1):
+            ax.bar(values.index, values)
+            ax.set_xticklabels([])
+            ax.set_xlabel("Genomes")
+            ax.set_ylabel("No. of CDS features")
+        else:
+            ax[i].bar(values.index, values)
+            ax[i].set_xticklabels([])
+            ax[i].set_xlabel("Genomes")
+            ax[i].set_ylabel("No. of CDS features")
+    return fig
+
 if __name__ == "__main__":
     pathlib.Path("data/qc/trans_table/").mkdir(parents=True, exist_ok=True)
     pathlib.Path("plot/qc/trans_table/").mkdir(parents=True, exist_ok=True)
 
     archaea_tables = pd.DataFrame(dict(loadGlob("data/genomes/archaea/*", getTranslationTable))).T
     archaea_tables.to_csv("data/qc/trans_table/archaea.csv")
-    archaea_tables.count().plot(kind='bar', figsize=(19,6)).get_figure().savefig("plot/qc/trans_table/archaea.png")
-    archaea_tables.plot(kind='bar', figsize=(19, 6))
-
-    fig, ax = plt.subplots(1, len(archaea_tables.columns), sharey=True)
-    for i, table in enumerate(archaea_tables.columns):
-        print(table)
-        values = archaea_tables[table].sort_values()
-        if(len(archaea_tables.columns) == 1): ax.bar(values.index, values)
-        else: ax[i].bar(values.index, values)
-        ax.set_xticklabels([])
-    fig.savefig("plot/qc/trans_table/archaea-distribution.png")
+    archaea_tables.count().plot(kind='bar', figsize=(6,6)).get_figure().savefig("plot/qc/trans_table/archaea.png")
+    plotDistribution(archaea_tables).savefig("plot/qc/trans_table/archaea-distribution.png")
 
     bacteria_tables = pd.DataFrame(dict(loadGlob("data/genomes/bacteria/*", getTranslationTable))).T
     bacteria_tables.to_csv("data/qc/trans_table/bacteria.csv")
     bacteria_tables.count().plot(kind='bar', figsize=(19,6)).get_figure().savefig("plot/qc/trans_table/bacteria.png")
+    plotDistribution(bacteria_tables).savefig("plot/qc/trans_table/bacteria-distribution.png")
 
-    # Plots the number of CDS which use each translation table
-    fig, ax = plt.subplots(1, len(bacteria_tables.columns), sharey=True)
-    for i, table in enumerate(bacteria_tables.columns):
-        print(table)
-        values = bacteria_tables[table].sort_values()
-        if(len(bacteria_tables.columns) == 1): ax.bar(values.index, values)
-        else: ax[i].bar(values.index, values)
-        ax.set_xticklabels([])
-    fig.savefig("plot/qc/trans_table/bacteria_distribution.png")
+    pd.concat({
+        "Bacteria": bacteria_tables.count() / bacteria_tables.count().sum(),
+        "Archaea":  archaea_tables.count() / archaea_tables.count().sum()
+    }, axis = 1).plot(
+        kind="bar",
+        xlabel="Translation Table", ylabel="Frequency",
+        rot=0, logy=True
+    ).get_figure().savefig("plot/qc/trans_table/both.png", dpi=600)

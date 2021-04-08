5a26db73c6c44af3037cdb5e4be327eef1edaa0a
Sulaiman Sulaiman <ss2980@bath.ac.uk>
2021-04-08 21:54:21 +0100

    Dataset: Human Genome
    Downloading the full primary assembly (GRCh29) rather than the refseq CDS fasta
    as well as the annotations so we can find the CDSs
    
    Signed-off-by: Sulaiman Sulaiman <ss2980@bath.ac.uk>

--- buildDataset.sh
+++ buildDataset.sh
@@ -10,6 +10,34 @@ python scripts/DatasetDownload.py # Fetchs the metadata needed to select the ass
 python scripts/DatasetFilter.py   # Filters the assemblies based on the metadata
 python scripts/DatasetFetch.py    # Downloads the assemblies which have been selected
 
+echo
+echo "${header}Building Human Dataset${headerend}"
+echo
+
+url_human_seq="https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh38_latest/refseq_identifiers/GRCh38_latest_genomic.fna.gz"
+gz_human_seq="data/genomes/build/download/GRCh38_latest_genomic.fna.gz"
+fna_human_seq="data/genomes/build/out/GRCh38_latest_genomic.fna"
+
+url_human_annotation="https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh38_latest/refseq_identifiers/GRCh38_latest_genomic.gff.gz"
+gz_human_annotation="data/genomes/build/download/GRCh38_latest_genomic.gff3.gz"
+fna_human_annotation="data/genomes/build/out/GRCh38_latest_genomic.gff3"
+
+echo "Downloading Human Genome Sequence"
+curl -# $url_human_seq -o $gz_human_seq
+echo "Downloading Human Genome Annotation"
+curl -# $url_human_annotation -o $gz_human_annotation
+
+size=$(gzip -l $gz_human_seq  | grep -Eo '\b[[:digit:]]+\b ' | tail -n1)
+gzip -dc $gz_human_seq | tqdm --desc "Extracting Human Genome Sequence" --bytes --total $size > $fna_human_seq
+
+size=$(gzip -l $gz_human_annotation  | grep -Eo '\b[[:digit:]]+\b ' | tail -n1)
+gzip -dc $gz_human_annotation | tqdm --desc "Extracting Human Genome Annotation" --bytes --total $size > $fna_human_annotation
+
+
+echo
+echo "${header}Finalising Dataset${headerend}"
+echo
+
 echo "Copying files to final location"
 cp -r data/genomes/build/out/* data/genomes/
 

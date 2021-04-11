d7f5188c13280fccd12174e86a113cca95a683db
Sulaiman Sulaiman <ss2980@bath.ac.uk>
2021-04-11 21:05:41 +0100

    Human Stop: List of CDSs
    Right now we can load all the data form from each file and combine them to
    produce a file containing usage data about each CDS
    
    Signed-off-by: Sulaiman Sulaiman <ss2980@bath.ac.uk>

--- scripts/analysis/stopUsageHuman.py
+++ scripts/analysis/stopUsageHuman.py
@@ -1,18 +1,44 @@
-
 import sys, pathlib
 sys.path.append(str(pathlib.Path(__file__).parent.parent.absolute()))
+
+from IPython import get_ipython
+if(get_ipython() != None):
+    import os
+    os.chdir("../..")
+
 from common import Parallel
-
-import pandas as pd
-
+from functools import partial
 from Bio import SeqIO, SeqUtils
 from Bio.SeqFeature import FeatureLocation
 
-def LoadRecord(feature, toFind = "CDS"):
-    data = []  # We will store the data we generate here
+import gffutils
+from gffutils.biopython_integration import to_seqfeature
 
-    for shift in range(0, 1): # FIXME: With the fasta file we don't have access to the sequence around the genome
-        seq = feature.seq # Extract the DNA sequence using the location
+import pandas as pd
+from tqdm.auto import tqdm
+
+# Load annotations and sequences
+
+annotation = gffutils.FeatureDB("data/genomes/GRCh38_latest_genomic.gff3.sqlite")
+sequence = list(tqdm(
+    SeqIO.parse("data/genomes/GRCh38_latest_genomic.fna", "fasta"),
+    desc = "Loading FASTA Sequences"
+))
+# Select for CDSs and process
+CDSs = list(tqdm(
+    annotation.features_of_type("CDS"),
+    desc  = "Loading CDS Annotations",
+    total = annotation.count_features_of_type("CDS")
+))
+
+def processFeature(cds):
+    chromosome = next(filter(lambda x: x.id == cds.chrom, sequence))
+    feature    = to_seqfeature(cds)
+
+    data = []
+    for shift in range(0, 6):
+        loc = feature.location + shift # Add the shift the "in-frame" ORF location
+        seq = loc.extract(chromosome.seq)  # Extract the DNA sequence using the location
 
         # Need to ensure we have a stop codon, otherwise we don't care
         if seq[-3:] in ["TAA", "TGA", "TAG"]:
@@ -21,18 +47,13 @@ def LoadRecord(feature, toFind = "CDS"):
                 "gc": SeqUtils.GC123(seq), # GC (incl. GC123) of sequence (given the shift)
                 "stop": str(seq[-3:])      # What stop codon we found
             })
+    return (feature.id, pd.DataFrame.from_dict(data)) # Pair the data with the record ID
 
-    return (feature.id, data) # Pair the data with the record ID
-
-
-def concatPreprocess(data): return { k:pd.DataFrame(v) for k,v in data }
-
-if __name__ == "__main__":
-    data = list(SeqIO.parse("data/genomes/human.fna", "fasta"))
-
-    result = Parallel.loadParallel(LoadRecord, data, len(data), desc = "Processing Human Genome")
-    result = Parallel.concat(result, concatPreprocess)
-    result.to_json("data/gc/cds/human.json", orient="table")
+results = Parallel.loadParallel(
+    processFeature, CDSs,
+    len(CDSs),
+    desc = "Stop Usage in Human Genome"
+)
 
 # Humans have high GC due to high recombintation rates + GC Conversion
 # Check for premature stops

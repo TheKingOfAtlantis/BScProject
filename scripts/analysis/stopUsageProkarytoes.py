9bbc0d5e8d64060ab9ac86a264039f241901858f
Sulaiman Sulaiman <ss2980@bath.ac.uk>
2021-04-11 18:04:46 +0100

    Prokaryote Stop Usage: Fixing problems
    Fixed:
     - Was passing list on each loop: Meant output contained results for both tRNA and
       CDS => also meant double the work for no reason
     - isInvalidFeature: When the list of matches was empty was not negating the truth
       value so would end up indicating if a feature was valid rather than invalid,
       but simply performing a NOT operation we can inverse this
    Signed-off-by: Sulaiman Sulaiman <ss2980@bath.ac.uk>

--- scripts/analysis/stopUsageProkarytoes.py
+++ scripts/analysis/stopUsageProkarytoes.py
@@ -7,11 +7,11 @@ from common import Filesystem, Parallel, getID
 
 import pandas as pd
 
-invalidCDS = pd.read_csv("data/qc/proteins/failed.csv").drop(columns=["Unnamed: 0", "index"])
+invalidCDS = pd.read_csv("data/qc/proteins/failed.csv")
 def isInvalidFeature(genome, feature):
-    rows = invalidCDS[invalidCDS["genome"] == genome]
-    rows = rows[rows["id"] == getID(feature)]
-    return rows.empty
+    rows = invalidCDS[invalidCDS.genome == genome]
+    rows = rows[rows.id == getID(feature)]
+    return not rows.empty
 
 def LoadRecord(file, toFind):
     record = next(SeqIO.parse(file, "embl")) # Load the record from the file
@@ -22,16 +22,15 @@ def LoadRecord(file, toFind):
 
         # Check feature against the list of invalid features
         # Found from QCing
-        # FIXME: I seem to be causing issues with the results just being random noise effectively
-        # if(feature.type == "CDS"):
-        #     if(isInvalidFeature(record.id, feature)):
-        #         continue # Skip this feature
+        if(feature.type == "CDS"):
+            if(isInvalidFeature(record.id, feature)):
+                continue # Skip this feature
 
         # We want to explore value across frameshifts
         # So shift the frame over 2 codons worth of nucleotides
         for shift in range(0, 6): # Check across 2 codons
-            loc   = feature.location + shift # Add the shift the "in-frame" ORF location
-            seq = loc.extract(record.seq)    # Extract the DNA sequence using the location
+            loc = feature.location + shift # Add the shift the "in-frame" ORF location
+            seq = loc.extract(record.seq)  # Extract the DNA sequence using the location
 
             # Need to ensure we have a stop codon, otherwise we don't care
             if seq[-3:] in ["TAA", "TGA", "TAG", "TAC"]:
@@ -43,21 +42,23 @@ def LoadRecord(file, toFind):
     return (record.id, data) # Pair the data with the record ID
 
 def concatPreprocess(data): return { k:pd.DataFrame(v) for k,v in data }
-if __name__ == "__main__":
-    toFind = ["CDS", "tRNA"]
 
+toFind = ["CDS", "tRNA"]
+
+for geneType in toFind:
     # Make sure we have the path to export to (including parents)
     Filesystem.mkdir(f"data/gc/{geneType.lower()}")
 
-    archaea_gene_gc = Filesystem.loadGlob("data/genomes/archaea/*", LoadRecord, toFind=toFind, desc = f"Processing Archaea {geneType}")
+    archaea_gene_gc = Filesystem.loadGlob(
+        "data/genomes/archaea/*", LoadRecord,
+        toFind=geneType,
+        desc = f"Archaea {geneType} Stop Usage"
+    )
     Parallel.concat(archaea_gene_gc, concatPreprocess).to_json(f"data/gc/{geneType.lower()}/archaea.json", orient="table")
 
-    bacteria_gene_gc = Filesystem.loadGlob("data/genomes/bacteria/*", LoadRecord, toFind=toFind, desc = f"Processing Bacteria {geneType}")
+    bacteria_gene_gc = Filesystem.loadGlob(
+        "data/genomes/bacteria/*", LoadRecord,
+        toFind=geneType,
+        desc = f"Bacteria {geneType} Stop Usage"
+    )
     Parallel.concat(bacteria_gene_gc, concatPreprocess).to_json(f"data/gc/{geneType.lower()}/bacteria.json", orient="table")
-
-
-# def LoadRecord(file, toFind = "CDS"):
-#     record = next(SeqIO.parse(file, "embl")) # Load the record from the file
-#     record.
-#  if __name__ == "__main__":
-#     archaea_gene_gc = loadGlob("data/genomes/*/*", LoadRecord)

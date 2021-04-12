88a672f17650a7946ca220c9ca6582f088a8bc09
Sulaiman Sulaiman <ss2980@bath.ac.uk>
2021-04-12 19:38:05 +0100

    Human Stop Usage: All logistic regression plots in one
    Combined all the different logisitic regression plots into one figure
    
    Added:
     - Regression includes constant column
     - Specified axes labels and ticks
     - Data dataframe
     - Automatically adds new row if we add another codon to plot
    Signed-off-by: Sulaiman Sulaiman <ss2980@bath.ac.uk>

--- scripts/analysis/stopUsagePlotLogistic.py
+++ scripts/analysis/stopUsagePlotLogistic.py
@@ -1,31 +1,37 @@
-import sys, pathlib
-sys.path.append(str(pathlib.Path(__file__).parent.parent.absolute()))
-from common import Filesystem
-
 import pandas as pd
 import numpy as np
 
-import statsmodels.api as sm
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
-human = pd.read_json("./data/gc/cds/human.json", orient = "table")
+human = pd.read_json("data/gc/cds/human.json", orient = "table")
 human[["gc", "gc1", "gc2", "gc3"]] = pd.DataFrame(human.gc.tolist(), index=human.index)
-human = human[["shift", "stop", "gc", "gc3"]]
+human = human[["shift", "stop", "gc3"]]
 
 human = human.reset_index().rename(columns={"level_0": "id"}).drop("level_1", axis=1)
 
-for stop in ['TAA', 'TAG', 'TGA']:
-    human["freq"] = human["stop"] == stop
-    result = sm.Logit(human["freq"], human["gc3"]).fit()
+codons = ['TAA', 'TGA', 'TAG']
+fig, axes = plt.subplots(nrows = len(codons), figsize = (16, 9), sharex=True)
+for ax, stop in zip(axes, codons):
+    data = pd.DataFrame({
+        "x": human.gc3,
+        "y": human.stop == stop # Whether each CDS has the stop we are checking for
+    })
 
     print(f"Stop {stop}:")
-    print(result.summary2())
 
-    fig, ax = plt.subplots(figsize = (16, 9))
-    ax.scatter(human['gc3'],human["freq"])
-    predict_x = np.linspace(human['gc3'].min(), human['gc3'].max())
-    ax.plot(predict_x,result.predict(predict_x), "-")
+    # Log logisitic regression curve
+    fit = sm.Logit(data.y, sm.add_constant(data.x)).fit()
+    predict_line = np.linspace(data.x.min(), data.x.max())
+    ax.plot(predict_line, fit.predict(sm.add_constant(predict_line)), "-")
 
-    Filesystem.mkdir("plot/gc/cds/")
-    plt.savefig(f"plot/gc/cds/human-logit-{stop}.png")
-    plt.close()
+    ax.set_ylabel(f"{stop} Used")
+
+    ax.set_yticks([0, 1])
+    ax.set_yticklabels(["False", "True"])
+
+    print(fit.summary2())
+
+axes[-1].set_xlabel("Gene GC3 Content (%)")
+plt.savefig(f"plot/gc/cds-human-logit.png")
